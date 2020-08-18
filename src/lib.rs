8ff476340a481b023c2272ae3ad1ff9cb4986e1a
9motom6 <9motom6@gmail.com>
2020-08-18 18:17:31 +0200

    Change rectangle into a square that is always a square

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 use web_sys::*;
 use web_sys::WebGlRenderingContext as GL;
 
+#[macro_use]
+extern crate lazy_static;
+
+mod app_state;
 mod gl_setup;
 mod shaders;
 mod common_funcs;
@@ -36,14 +41,22 @@ impl GraphicsClient {
     }
 
     pub fn update(&mut self, time: f32, height: f32, width: f32) -> Result<(), JsValue> {
+        app_state::update_dynamic_data(time, height, width);
         Ok(())
     }
 
     pub fn render(&self) {
         self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+        let current_state = app_state::get_curr_state();
 
         self.program_color_2d.render(
-            &self.gl, 3., 10., 5., 10., 20., 30.,
+            &self.gl,
+            current_state.control_bottom,
+            current_state.control_top,
+            current_state.control_left,
+            current_state.control_right,
+            current_state.canvas_height,
+            current_state.canvas_width,
         )
     }
 }
